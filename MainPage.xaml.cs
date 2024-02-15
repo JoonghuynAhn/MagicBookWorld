edf69c51c92326d6eab394c7041e0890e8470108
JoonghuynAhn <134945269+JoonghuynAhn@users.noreply.github.com>
2024-02-15 15:00:08 -0500

    Splash Update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Plugin.Maui.Audio;
+﻿using CommunityToolkit.Maui.Views;
 
 namespace MagicBookWorld;
 
@@ -10,26 +10,65 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 		//imgButton1.Source = ImageSource.FromFile("dog.png");
-		imgButton1.Source = "dog.png";
+		//imgButton1.Source = "dog.png";
 		hockey.Source = "hockey0.png";
-		scientist.Source = "femalescientist1.png";
+		scientist.Source = "femalescientist2.png";
 		tomadventure.Source = "tomadventure1.png";
 
+        //PlaySound(correctMediaElement);
 
 
     }
+    //internal static void PlaySound(MediaElement me)
+    //{
+    //    var preset = MauiProgram.CreateMauiApp;
+    //    if (me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped &&
+    //        me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused)
+    //    {
+    //        me.Stop();
+    //    }
+    //    if (preset != null)
+    //    {
+    //        switch (preset.BuzzerVolume)
+    //        {
+    //            case Volume.Off:
+    //                me.Volume = 0.0;
+    //                break;
+    //            case Volume.Low:
+    //                me.Volume = 0.2;
+    //                break;
+    //            case Volume.Medium:
+    //                me.Volume = 0.7;
+    //                break;
+    //            case Volume.High:
+    //                me.Volume = 1.0;
+    //                break;
+    //            default:
+    //                break;
+    //        }
+    //    }
 
-	//private void OnCounterClicked(object sender, EventArgs e)
-	//{
-	//	count++;
+    //    if (me.Position != TimeSpan.Zero)
+    //    {
+    //        //Will Play sound here
+    //        me.SeekTo(TimeSpan.Zero);
+    //    }
+    //    else if (me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+    //    {
+    //        me.Play();
+    //    }
+    //}
+    //private void OnCounterClicked(object sender, EventArgs e)
+    //{
+    //	count++;
 
-	//	if (count == 1)
-	//		CounterBtn.Text = $"Clicked {count} time";
-	//	else
-	//		CounterBtn.Text = $"Clicked {count} times";
+    //	if (count == 1)
+    //		CounterBtn.Text = $"Clicked {count} time";
+    //	else
+    //		CounterBtn.Text = $"Clicked {count} times";
 
-	//	SemanticScreenReader.Announce(CounterBtn.Text);
-	//}
+    //	SemanticScreenReader.Announce(CounterBtn.Text);
+    //}
 
     private void ImageButton_Clicked(object sender, EventArgs e)
     {
