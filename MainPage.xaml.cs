2f30f2deec06616301a3b962185576c621edfe53
JoonghuynAhn <134945269+JoonghuynAhn@users.noreply.github.com>
2024-02-15 15:40:19 -0500

    Background / Main Page Update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,62 +13,11 @@ public partial class MainPage : ContentPage
 		//imgButton1.Source = "dog.png";
 		hockey.Source = "hockey0.png";
 		scientist.Source = "femalescientist2.png";
-		tomadventure.Source = "tomadventure1.png";
+		tomadventure.Source = "tomadventure2.png";
 
-        //PlaySound(correctMediaElement);
-
+        mediaElement.Source = MediaSource.FromResource("LittleSamba.mp3");
 
     }
-    //internal static void PlaySound(MediaElement me)
-    //{
-    //    var preset = MauiProgram.CreateMauiApp;
-    //    if (me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Stopped &&
-    //        me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Paused)
-    //    {
-    //        me.Stop();
-    //    }
-    //    if (preset != null)
-    //    {
-    //        switch (preset.BuzzerVolume)
-    //        {
-    //            case Volume.Off:
-    //                me.Volume = 0.0;
-    //                break;
-    //            case Volume.Low:
-    //                me.Volume = 0.2;
-    //                break;
-    //            case Volume.Medium:
-    //                me.Volume = 0.7;
-    //                break;
-    //            case Volume.High:
-    //                me.Volume = 1.0;
-    //                break;
-    //            default:
-    //                break;
-    //        }
-    //    }
-
-    //    if (me.Position != TimeSpan.Zero)
-    //    {
-    //        //Will Play sound here
-    //        me.SeekTo(TimeSpan.Zero);
-    //    }
-    //    else if (me.CurrentState != CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
-    //    {
-    //        me.Play();
-    //    }
-    //}
-    //private void OnCounterClicked(object sender, EventArgs e)
-    //{
-    //	count++;
-
-    //	if (count == 1)
-    //		CounterBtn.Text = $"Clicked {count} time";
-    //	else
-    //		CounterBtn.Text = $"Clicked {count} times";
-
-    //	SemanticScreenReader.Announce(CounterBtn.Text);
-    //}
 
     private void ImageButton_Clicked(object sender, EventArgs e)
     {
